a153370becf9ab6632a220f7d68dbdda7bdcbbd7
Kandpal, Rakesh <Rakesh_Kandpal@intuit.com>
2017-05-17 11:07:52 -0700

    initial commit

---
+++ swift_play.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//variable declaration:
+//infere typing -> swift infere type on it's own
+var str = "Hello, playground"
+
+//function
+print (str)
+
+//since we used double quptes this will be infered as string if not explicity decalred string
+var myMiddleInitial: Character = "A"
+var numberOfWheels: Int = 4
+var centimetersOfRainfall: Float = 5.5
+var pi: Double = 3.14159265359
+var letterOfTheDay: Character = "z"
+var myFavoriteAnimal: String  = "nudibranch"
+var rainingOutside: Bool  = true
+
+
+//var myNumber: Int   ===  int myNumber; (java)
+
+
+//A constant's value can't change
+let birthYear = 2008
+
+
